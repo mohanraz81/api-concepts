df6f1be6bc4ca5cb3ad1d48f51a68e06e76092e8
mohanraz81 <mohanraz@gmail.com>
2023-10-17 09:29:54 +0530

    Update addtocart.py

--- addtocart.py
+++ addtocart.py
@@ -1,33 +1,19 @@
 import json
 import boto3
-
-# Initialize a DynamoDB client
-dynamodb = boto3.client('dynamodb')
-
-# Define the DynamoDB table name
-table_name = 'CartTable'
-
 def lambda_handler(event, context):
     # Extract request data from the event
-    request_body = json.loads(event['body'])
-    user_id = request_body['userId']
-    cart_items = request_body['cartItems']
-
+    print(event)
+    # user_id = event['userid']
+    # cart_items = event['cartitems']
+    TableName = 'CartTable'
+    dynamodb=boto3.resource('dynamodb',region_name='us-east-1')
+    table=dynamodb.Table(TableName)
     # Update the user's cart in DynamoDB
-    response = dynamodb.update_item(
-        TableName=table_name,
-        Key={
-            'UserId': {'S': user_id}
-        },
-        UpdateExpression='SET Cart = :cart',
-        ExpressionAttributeValues={
-            ':cart': {'S': json.dumps(cart_items)}
-        }
-    )
-
+  
+    response=table.put_item(Item=event)
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
         return {
-            'statusCode': 200,
+            'statusCode': 201,
             'body': json.dumps({'message': 'Cart updated successfully'}),
             'headers': {
                 'Content-Type': 'application/json'
